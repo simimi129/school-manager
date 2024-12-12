60c21e99957f4206347538790b8f8865d2675d4b
simimi129 <simimi129@gmail.com>
2024-12-12 19:15:26 +0100

    adding auth0 environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  auth0: {
+    domain: 'dev-y8ri6abm4fwrifio.us.auth0.com',
+    clientId: 'tX5sRmbdPHgbRXH9JSG6cBaFBBB8Hmlb',
+    redirectUri: 'http://localhost:4200',
+  },
+};
