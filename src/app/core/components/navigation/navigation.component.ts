3f88b184e63c101e3e7a1ed5bf18b22ae753ae19
simimi129 <simimi129@gmail.com>
2025-01-11 14:11:29 +0100

    adding active router link color

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LinkComponent } from '../../../shared/components/link/link.component';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navigation',
-  imports: [ButtonComponent, LinkComponent, MatTooltipModule],
+  imports: [ButtonComponent, LinkComponent, MatTooltipModule, MatButtonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/components/link/link.component.ts
+++ src/app/shared/components/link/link.component.ts
@@ -1,9 +1,9 @@
 import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-link',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './link.component.html',
   styleUrl: './link.component.scss',
 })
