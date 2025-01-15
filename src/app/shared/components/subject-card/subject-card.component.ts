32091230f986c382d1c92b88acb6f116318d109f
simimi129 <simimi129@gmail.com>
2025-01-15 20:21:16 +0100

    styling subject-card

--- src/app/shared/components/subject-card/subject-card.component.ts
+++ src/app/shared/components/subject-card/subject-card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-subject-card',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './subject-card.component.html',
-  styleUrl: './subject-card.component.scss'
+  styleUrl: './subject-card.component.scss',
 })
 export class SubjectCardComponent {
-
+  title = input('');
+  code = input('');
+  gpa = input();
+  color = input('');
 }
