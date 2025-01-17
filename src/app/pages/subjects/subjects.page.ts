4fa9a264d5cd6a951e77d1165d0872f420f70d37
simimi129 <simimi129@gmail.com>
2025-01-17 12:04:49 +0100

    adding drag and drop basics

--- src/app/pages/subjects/subjects.page.ts
+++ src/app/pages/subjects/subjects.page.ts
@@ -1,19 +1,41 @@
-import { Component, DestroyRef, inject } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { SubjectCardComponent } from '../../shared/components/subject-card/subject-card.component';
 import { SubjectsHttpService } from './data-access/http/subjects-http.service';
-import { AsyncPipe } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { SubjectModel } from './data-access/models/subjects.interfaces';
+import { BehaviorSubject, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-subjects',
-  imports: [SubjectCardComponent, AsyncPipe],
+  imports: [SubjectCardComponent, AsyncPipe, CdkDropList, CdkDrag],
   templateUrl: './subjects.page.html',
   styleUrl: './subjects.page.scss',
 })
-export class SubjectsComponent {
+export class SubjectsComponent implements OnInit {
   private subjectsHttpService = inject(SubjectsHttpService);
   private destroyRef = inject(DestroyRef);
-  subjects = this.subjectsHttpService
-    .get()
-    .pipe(takeUntilDestroyed(this.destroyRef));
+  subjectsForDragAndDrop = new BehaviorSubject([] as SubjectModel[]);
+  subjects$ = this.subjectsForDragAndDrop.asObservable();
+
+  ngOnInit(): void {
+    this.subjects$ = this.subjectsHttpService.get().pipe(
+      tap((subjects: SubjectModel[]) =>
+        this.subjectsForDragAndDrop.next(subjects)
+      ),
+      takeUntilDestroyed(this.destroyRef)
+    );
+  }
+
+  drop(event: CdkDragDrop<SubjectModel[] | null>) {
+    const items = this.subjectsForDragAndDrop.getValue();
+    moveItemInArray(items, event.previousIndex, event.currentIndex);
+    this.subjectsForDragAndDrop.next(items);
+  }
 }
