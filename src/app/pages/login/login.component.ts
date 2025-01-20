9f83e8b739f407d35fc3078eb121c0df9c16926d
simimi129 <simimi129@gmail.com>
2025-01-20 12:40:35 +0100

    adding fake login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../core/services/auth/services/auth.service';
+import { combineLatest, filter, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
@@ -17,14 +21,25 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class LoginComponent {
   private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
   loginForm = this.formBuilder.group({
     email: ['', Validators.required, Validators.email],
     password: ['', Validators.required],
   });
 
-  onSubmit() {
-    // TODO: validate
-    console.log(this.loginForm.value);
+  login() {
+    this.authService.login('test@test.com', 'password');
+    combineLatest([this.authService.authStatus$, this.authService.currentUser$])
+      .pipe(
+        filter(
+          ([authStatus, user]) => authStatus.isAuthenticated && user._id !== ''
+        ),
+        tap(() => this.router.navigate(['/timetable'])),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
   }
 }
