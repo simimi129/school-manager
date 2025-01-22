50bbac942bd3d447c0e9519a39d7e59133aeda11
simimi129 <simimi129@gmail.com>
2025-01-22 09:48:24 +0100

    cleaning env files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,3 @@
 export const environment = {
   baseUrl: 'http://localhost:4200',
-  auth0: {
-    domain: 'dev-y8ri6abm4fwrifio.us.auth0.com',
-    clientId: 'tX5sRmbdPHgbRXH9JSG6cBaFBBB8Hmlb',
-    redirectUri: 'http://localhost:4200',
-  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
 export const environment = {
   baseUrl: 'edit for production',
-  auth0: {
-    domain: 'dev-y8ri6abm4fwrifio.us.auth0.com',
-    clientId: 'tX5sRmbdPHgbRXH9JSG6cBaFBBB8Hmlb',
-    redirectUri: 'http://localhost:4200',
-  },
 };
